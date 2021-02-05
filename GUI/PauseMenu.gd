5a54cde69a135da47cc00d88f267131c78b7a90a
ColinJWright <colinwright2771@gmail.com>
2021-02-05 16:23:02 +0000

    Lesson 107 complete. Added a Pause Menu popup.

---
+++ GUI/PauseMenu.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+func _input(event):
+	if Input.is_action_just_pressed("Pause"):
+		get_tree().paused = !get_tree().paused
+		toggle_pause_menu()
+
+func toggle_pause_menu():
+	if get_tree().paused:
+		$Popup.show()
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		$Popup.hide()
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _on_FullScreenButton_pressed():
+	OS.window_fullscreen = !OS.window_fullscreen
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
